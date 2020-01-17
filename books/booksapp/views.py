f639e02b3fbe67d1c9bb06509daf3ac113eb84b0
wahyudi <wahyudi.coding@gmail.com>
2020-01-18 01:13:57 +0700

    list html

--- books/booksapp/views.py
+++ books/booksapp/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
+
+from .models import Book
 
 # Create your views here.
+class HomeView(TemplateView):
+    template_name = 'home.html'
+
+    def get(self, request):
+        return render(request, self.template_name)
+
+
+class ListView(TemplateView):
+    template_name = 'books_list.html'
+
+    def get(self, request):
+        books = Book.objects.all()
+
+        return render(request, self.template_name, {'books':books})
